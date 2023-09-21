fbb910e3255ac1d2082e388a2f33ff0256f32cbe
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-09-21 17:46:06 +0200

    vhdl-ts-mode: Update ref files after change in indent_misc.vhd

--- vhdl/files/imenu/vhdl-ts-mode/ref/indent_misc.el
+++ vhdl/files/imenu/vhdl-ts-mode/ref/indent_misc.el
@@ -1,5 +1,5 @@
-(("foo (ent)" . #<marker at 332 in indent_misc.vhd>)
+(("foo (ent)" . #<marker at 434 in indent_misc.vhd>)
  ("indent (arch)..."
-  ("*architecture declaration*" . #<marker at 395 in indent_misc.vhd>)
-  ("var (proc)" . #<marker at 441 in indent_misc.vhd>))
- ("test_multiline_expression (pcd)" . #<marker at 840 in indent_misc.vhd>))
+  ("*architecture declaration*" . #<marker at 525 in indent_misc.vhd>)
+  ("var (proc)" . #<marker at 571 in indent_misc.vhd>))
+ ("test_multiline_expression (pcd)" . #<marker at 970 in indent_misc.vhd>))
