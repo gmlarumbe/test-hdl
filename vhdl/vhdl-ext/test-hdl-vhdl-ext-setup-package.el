cff4f290e1697dfde68948a0cd35cdf7b6aac478
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2024-04-06 23:42:49 +0200

    vhdl-ext: add lspce setup code

--- vhdl/vhdl-ext/test-hdl-vhdl-ext-setup-package.el
+++ vhdl/vhdl-ext/test-hdl-vhdl-ext-setup-package.el
@@ -46,6 +46,7 @@
 (require 'vhdl-ext)
 (setq vhdl-ext-cache-enable nil)
 (setq vhdl-ext-feature-list (remove 'lsp-bridge vhdl-ext-feature-list)) ; Do not autosetup since `lsp-bridge' is not available on MELPA
+(setq vhdl-ext-feature-list (remove 'lspce vhdl-ext-feature-list)) ; Do not autosetup since `lspce' is not available on MELPA
 (vhdl-ext-mode-setup)
 (add-hook 'vhdl-mode-hook #'vhdl-ext-mode)
 
